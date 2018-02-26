ce30f89605c576a6d722ed6510535df6a67c201b
墨迹兔 <mojitu@outlook.com>
2018-02-27 02:54:52 +0800

    create

---
+++ SystemDebuger/SystemDebuger/Debuger.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace SystemDebuger
+{
+    public class Debuger
+    {
+        public static bool debugEnable = false;
+
+        public static void Log(object message)
+        {
+            Log(message, null);
+        }
+        public static void Log(object message, UnityEngine.Object context)
+        {
+            if (debugEnable)
+            {
+                Debug.Log(message, context);
+            }
+        }
+
+        public static void LogWarning(object message)
+        {
+            LogWarning(message, null);
+        }
+        public static void LogWarning(object message, UnityEngine.Object context)
+        {
+            if (debugEnable)
+            {
+                Debug.LogWarning(message, context);
+            }
+        }
+
+        public static void LogError(object message)
+        {
+            LogError(message, null);
+        }
+        public static void LogError(object message, UnityEngine.Object context)
+        {
+            if (debugEnable)
+            {
+                Debug.LogError(message, context);
+            }
+        }
+    }
+}
